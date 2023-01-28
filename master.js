3fb6f6a6d33fd3c72636fa5c9af6fac482cf254f
RDCamu <roylan.camu@gmail.com>
2023-01-28 22:08:11 +0800

    Updated Navigation

--- master.js
+++ master.js
@@ -1,15 +1,22 @@
 const menubtn = document.querySelector('.openbtn')
 const menu = document.querySelector('.primary-navigation')
-const closebtn = document.querySelector('.close')
-
+const menuimg = document.getElementById("menuimg")
+const backdrop = document.querySelector('.backdrop')
 
 
 
 menubtn.addEventListener('click', () => {
     menu.classList.toggle('inposition')
+
+    if (menu.classList.contains('inposition')) {
+        menuimg.setAttribute("src","./assets/images/icon-menu-close.svg")
+        backdrop.classList.add('visible')
+    } else {
+        menuimg.setAttribute("src", "./assets/images/icon-menu.svg")
+        backdrop.classList.remove('visible')
+    }
+
+
 })
 
-closebtn.addEventListener('click', () => {
-    menu.classList.remove('inposition')
-})
 
